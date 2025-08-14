a0df78791e7753aa89b0ab0b8493e320ace90af9
Julangar <jagarciaguer@msn.com>
2025-08-14 16:24:55 -0500

    feat: update Java and Kotlin compatibility to version 17 and enable core library desugaring

--- mine-chat-app/mobile/android/app/build.gradle.kts
+++ mine-chat-app/mobile/android/app/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     // https://firebase.google.com/docs/android/setup#available-libraries
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 android {
@@ -26,12 +27,13 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
